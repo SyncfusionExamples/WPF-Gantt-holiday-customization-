c4fdf716be919b6fcaf8534295526e31573bd205
sheik syed abthaheer <sheiksyedm@syncfusion.com>
2019-06-17 16:00:03 +0530

    Samples added for gantt holiday customization support.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+
+namespace CalendarCustomization
+{
+    //using System;
+    using System.Windows;
+    //using System.Windows.Media;
+    //using Syncfusion.Windows.Controls.Gantt;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ViewModel viewModel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            //viewModel = new ViewModel();
+
+            //GanttControl gantt = new GanttControl();
+            //gantt.ItemsSource = this.viewModel.TaskCollection;
+            //gantt.VisualStyle = VisualStyle.Metro;
+            //gantt.ShowHolidays = true;
+            //gantt.ExcludeHolidays = false;
+            //gantt.Holidays = new GanttHolidayCollection
+            //{
+            //    new GanttHoliday { Day = new DateTime(2010, 6, 10), Background = Brushes.CadetBlue },
+            //    new GanttHoliday { Day = new DateTime(2010, 7, 8), Background = Brushes.CadetBlue },
+            //    new GanttHoliday { Day = new DateTime(2010, 8, 3), Background = Brushes.CadetBlue },
+            //    new GanttHoliday { Day = new DateTime(2010, 9, 20), Background = Brushes.CadetBlue }
+            //};
+
+            //this.AddChild(gantt);
+        }
+    }
+}
